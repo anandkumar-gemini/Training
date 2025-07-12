7c6afaf413b7df800a863ec0316e44251a8cd46a
anandkumar-gemini <anandkumar.gemini@gmail.com>
2025-07-12 15:42:34 -0500

    Update train.py

--- data-science/src/train.py
+++ data-science/src/train.py
@@ -4,13 +4,18 @@
 Trains ML model using the training dataset and evaluates using the test dataset. Saves trained model.
 """
 
-import argparse
-from pathlib import Path
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
 import mlflow
-import mlflow.sklearn
+import argparse
+import os
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_transformer
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, recall_score
+from sklearn.ensemble import RandomForestRegressor 
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from matplotlib import pyplot as plt
 
 def parse_args():
@@ -20,7 +25,7 @@ def parse_args():
     parser.add_argument("--train_data", type=str, help="Path to train dataset")
     parser.add_argument("--test_data", type=str, help="Path to test dataset")
     parser.add_argument("--model_output", type=str, help="Path of output model")
-    parser.add_argument('--criterion', type=str, default='gini',
+    parser.add_argument('--n_estimators', type=int, default='gini',
                         help='The function to measure the quality of a split')
     parser.add_argument('--max_depth', type=int, default=None,
                         help='The maximum depth of the tree. If None, then nodes are expanded until all the leaves contain less than min_samples_split samples.')
@@ -37,29 +42,33 @@ def main(args):
     test_df = pd.read_csv(Path(args.test_data)/"test.csv")
 
     # Split the data into input(X) and output(y)
-    y_train = train_df['Failure']
-    X_train = train_df.drop(columns=['Failure'])
-    y_test = test_df['Failure']
-    X_test = test_df.drop(columns=['Failure'])
+    y_train = train_df['price']
+    X_train = train_df.drop(columns=['price'])
+    y_test = test_df['price']
+    X_test = test_df.drop(columns=['price'])
 
-    # Initialize and train a Decision Tree Classifier
-    model = DecisionTreeClassifier(criterion=args.criterion, max_depth=args.max_depth)
+    # Initialize and train a Random forest regressor
+    model = RandomForestRegressor(n_estimators=args.n_estimators, max_depth=args.max_depth, random_state=42)
     model.fit(X_train, y_train)
 
     # Log model hyperparameters
-    mlflow.log_param("model", "DecisionTreeClassifier")
-    mlflow.log_param("criterion", args.criterion)
+    mlflow.log_param("model", "RandomForestRegressor")
+    mlflow.log_param("criterion", args.n_estimators)
     mlflow.log_param("max_depth", args.max_depth)
 
     # Predict using the Decision Tree Model on test data
     yhat_test = model.predict(X_test)
 
-    # Compute and log accuracy score
-    accuracy = accuracy_score(y_test, yhat_test)
-    print(f'Accuracy of Decision Tree classifier on test set: {accuracy:.2f}')
-    # Logging the accuracy score as a metric
-    mlflow.log_metric("Accuracy", float(accuracy))
-
+    # Compute and log mean_squared_error
+    mse = mean_squared_error(y_test, tree_predictions)
+    r2 = r2_score(y_test, tree_predictions)
+    mae = mean_absolute_error(y_test, tree_predictions)
+    
+    print(f'Mean Squared Error: {mse:.2f}')
+    # Logging the R2 score as a metric
+    mlflow.log_metric("MSE", float(mse))
+    print(f'R2 Score: {r2:.2f}')
+    print(f'Mean Absolute Error: {mae:.2f}')
     # Save the model
     mlflow.sklearn.save_model(sk_model=model, path=args.model_output)
 
